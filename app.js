64a49a4a5a0295b8682c4cb2d2e2f8c2831a6fd0
Shailesh Sakhare <shailesh.sakhare.contractor@move.com>
2018-08-05 17:40:40 +0530

    Changes using yargs

--- app.js
+++ app.js
@@ -1,24 +1,67 @@
-console.log('Starting Main.js');
+console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
+const _ = require('lodash');
 const yargs = require('yargs');
+
 const notes = require('./notes.js');
 
-const argv = yargs.argv;
-const command = argv._[0];
+const titleOptions = {
+    describe : 'Title of Note',
+    demand : true,
+    alias : 't'
+}
 
-console.log(argv);
-console.log(command);
+const bodyOptions = {
+    describe : 'Body of the Note',
+    demand : true,
+    alias : 'b'
+}
 
-if(command == 'add'){
-    notes.addNote(argv.title, argv.body);
-}else if(command == 'read'){
-    notes.getNote(argv.title);
-}else if(command == 'list'){
-    notes.getList();
-}else if(command == 'remove'){
-    notes.removeNote(argv.title);
-}else{
-    console.log("Command Not Found");
+const argv = yargs
+  .command('add', 'Add a new Note', {
+    title: titleOptions,
+    body: bodyOptions
+  })
+  .command('list', 'list all nodes')
+  .command('read', 'Read a note', {
+    title: titleOptions
+  })
+  .command('remove', 'Remove a note', {
+    title: titleOptions
+  })
+  .help()
+  .argv;
+
+var command = argv._[0];
+console.log('Command: ', command);
+console.log('Yargs', argv);
+
+if (command === 'add') {
+  var note = notes.addNote(argv.title, argv.body);
+  if (note) {
+    console.log('Note created');
+    notes.logNote(note);
+  } else {
+    console.log('Note title taken');
+  }
+} else if (command === 'list') {
+  let allNotes = notes.getAll();
+  console.log(`We have ${allNotes.length} note(s)`);
+  allNotes.forEach(note => console.log(note));
+} else if (command === 'read') {
+  var note = notes.getNote(argv.title);
+  console.log(note);
+  if (note.length > 0) {
+    console.log('Note Found');
+    notes.logNote(note[0]);
+  } else {
+    console.log('Note Not Found');
+  }
+} else if (command === 'remove') {
+  let noteRemoved = notes.removeNote(argv.title);
+  let message = noteRemoved ? 'Note was Removed':'Note was Found'; 
+  console.log(message);
+} else {
+  console.log('Command not recognized');
 }
--- notes.js
+++ notes.js
@@ -1,26 +1,69 @@
-console.log('Starting Notes.js');
+console.log('Starting notes.js');
+
+const fs = require('fs');
+
+var fetchNotes = () => {
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    return JSON.parse(notesString);
+  } catch (e) {
+    return [];
+  }
+};
+
+var saveNotes = (notes) => {
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
 
 var addNote = (title, body) => {
-    console.log('Add One Note',title,body);
-    return 'New Note';
-};
-var removeNote = (title) => {
-    console.log('Remove One Note', title);
-    return 'Removed Note';
-};
-var getNote = (title) => {
-    console.log('get All Notes',title);
-    return 'Get All Note';
-};
-var getList = () => {
-    console.log('get All Notes');
-    return 'Get All Notes';
+  var notes = fetchNotes();
+  var note = {
+    title,
+    body
+  };
+  var duplicateNotes = notes.filter((note) => note.title === title);
+
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    saveNotes(notes);
+    return note;
+  }
 };
 
+var getAll = () => {
+  return fetchNotes();
+};
+
+var getNote = (title) => {
+  let notes = fetchNotes();
+  var note = {
+    title
+  };
+  var duplicateNotes = notes.filter((note) => note.title === title);
+  return duplicateNotes;
+};
+
+var removeNote = (title) => {
+  var notes = fetchNotes();
+  var note = {
+    title
+  };
+  var duplicateNotes = notes.filter((note) => note.title === title);
+  saveNotes(duplicateNotes);
+  return notes.length !== duplicateNotes.length;
+};
+
+var logNote = (note) => {
+  debugger;
+  console.log('--');
+  console.log(`Title: ${note.title}`);
+  console.log(`Body: ${note.body}`);
+}
 
 module.exports = {
-    addNote,
-    removeNote,
-    getNote,
-    getList
+  addNote,
+  getAll,
+  getNote,
+  removeNote,
+  logNote
 };
